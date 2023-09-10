78ec4a2dbde558447ffb1335032a25a07152b1ef
V. hs Sin <cbrnt1210@naver.com>
2023-09-10 13:18:22 +0900

    Update open_raw_file.py
    readme 기반 가로 세로 설정

--- open_raw_file.py
+++ open_raw_file.py
@@ -3,7 +3,7 @@ from PIL import Image
 # .raw 파일 경로 및 이미지 너비와 높이 설정
 file_path = "C:/sin/vtk/a_vm2560.raw"  # .raw 파일 경로
 width = 2048                     # 이미지 너비
-height = 2048                     # 이미지 높이
+height = 1216                     # 이미지 높이
 mode = "L"                       # 이미지 모드 (예: "L"는 흑백 이미지, CT용, RGB로 색 가능)
 
 # .raw 파일 열기
