7fabe37a1c326d4fe537e9bc998ffc524a583e68
이제영 <201702773@hufs.ac.kr>
2023-09-19 16:51:35 +0900

    자료

--- open_raw_file.py
+++ open_raw_file.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # .raw 파일 경로 및 이미지 너비와 높이 설정
-file_path = "C:/sin/vtk/a_vm2560.raw"  # .raw 파일 경로
+file_path = r"E:\sw연구_전교수님\추출이미지 raw파일\avf2050a.raw"  # .raw 파일 경로
 width = 2048                     # 이미지 너비
 height = 1216                     # 이미지 높이
 mode = "L"                       # 이미지 모드 (예: "L"는 흑백 이미지, CT용, RGB로 색 가능)
